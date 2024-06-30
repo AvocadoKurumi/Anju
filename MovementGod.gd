50a967146596e9445aec6a6c372b0011c298379c
AvocadoKurumi <165980820+AvocadoKurumi@users.noreply.github.com>
2024-06-30 09:05:51 +0800

    Yes

---
+++ MovementGod.gd
@@ -0,0 +1,20 @@
+extends Sprite2D
+
+var speed = 300 #300 pixels per second
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_action_pressed("ui_right") or Input.is_action_pressed("Right"):
+		position.x += speed * delta
+	if Input.is_action_pressed("ui_left") or Input.is_action_pressed("Left"):
+		position.x -= speed * delta
+	if Input.is_action_pressed("ui_up") or Input.is_action_pressed("Up"):
+		position.y -= speed * delta
+	if Input.is_action_pressed("ui_down") or Input.is_action_pressed("Down"):
+		position.y += speed * delta
+	
